10b77cec834c1f55676f29bfc11732f1119f92df
farajnazish-eic <168401218+farajnazish-eic@users.noreply.github.com>
2025-03-13 15:42:30 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,17 +5,17 @@ terraform {
       version = "4.1.0"
     }
     azapi = {
-      source = "azure/azapi"
+      source = "Azure/azapi"
     }
   }
 }
 
+provider "azapi" {
+}
 # Configure the Microsoft Azure Provider
 
 provider "azurerm" {
   features {}
   subscription_id                 = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
   resource_provider_registrations = "none"
-}
-provider "azapi" {
 }
