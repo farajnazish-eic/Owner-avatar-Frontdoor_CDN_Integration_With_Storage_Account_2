4e7f65e8386f329abc68f1537e1d319516df9d23
farajnazish-eic <168401218+farajnazish-eic@users.noreply.github.com>
2025-03-13 15:42:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
+# module "dns_zone" {
+#   source              = "./dns_zone"
+#   resource_group_name = var.resource_group_name
+# }
+
 module "storage_account" {
-  source = "./storage_account"
   resource_group_name = var.resource_group_name
+  source = "./storage_account"
 }
 
 module "frontdoor" {
   source              = "./frontdoor"
   resource_group_name = var.resource_group_name
+  # dns_zone_id         = module.dns_zone.dns_zone_id
   host_name           = module.storage_account.stoarage_account_static_primary_ep
   origin_host_header  = module.storage_account.stoarage_account_static_primary_ep
   stoarage_account_id = module.storage_account.stoarage_account_id
