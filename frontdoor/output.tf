5264a07b82793159d27a18d8ec2b501371d4b15b
farajnazish-eic <168401218+farajnazish-eic@users.noreply.github.com>
2025-03-13 15:37:42 +0530

    Update output.tf

--- frontdoor/output.tf
+++ frontdoor/output.tf
@@ -1,10 +1,10 @@
-output "frontdoor_private_endpoint_name" {
-  description = "The endpoint name of the frontdoor private endpoint"
-  value       = azurerm_cdn_frontdoor_endpoint.example.name
-}
+#output "frontdoor_private_endpoint_name" {
+#  description = "The endpoint name of the frontdoor private endpoint"
+#  value       = azurerm_cdn_frontdoor_endpoint.example.name
+#}
  
 
-output "frontdoor_private_endpoint_id" {
-  description = "The endpoint ID of the frontdoor private endpoint"
-  value       = azurerm_cdn_frontdoor_endpoint.example.id
-}
+#output "frontdoor_private_endpoint_id" {
+#  description = "The endpoint ID of the frontdoor private endpoint"
+#  value       = azurerm_cdn_frontdoor_endpoint.example.id
+#}
